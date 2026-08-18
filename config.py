44681eb56e6f1bcfb91532c77e2a097ccc0cbedc
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-08-19 03:28:51 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+BOT_TOKEN = "8280168009:AAGY8UacyFSOU1tgA2mkoqFCZZm91-dJCV0"
+
+ADMIN_ID = 1305881282
+
+ADMIN_IDS = [1305881282]  # ganti dengan Telegram ID kamu
+
+# Grup tempat admin verifikasi pembayaran
+
+PAYMENT_GROUP_ID = -1003934607716
+
+# Link Bot Signal
+
+SIGNAL_BOT = "https://t.me/AIGOLDASSISTANT_BOT?start"
