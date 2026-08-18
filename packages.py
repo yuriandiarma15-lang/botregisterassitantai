9e92633b754aef4aaa25563787a04373fd70a5dc
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-08-19 03:29:36 +0700

    Create packages.py

---
+++ packages.py
@@ -0,0 +1,51 @@
+# packages.py
+
+
+PACKAGE_MAP = {
+
+
+    "1month": {
+
+        "label": "1 Bulan",
+
+        "price": 250000,
+
+        "days": 30
+
+    },
+
+
+    "6month": {
+
+        "label": "6 Bulan",
+
+        "price": 500000,
+
+        "days": 180
+
+    },
+
+
+    "12month": {
+
+        "label": "12 Bulan",
+
+        "price": 850000,
+
+        "days": 365
+
+    },
+
+
+    "permanent": {
+
+        "label": "Permanent",
+
+        "price": 1500000,
+
+        "days": 9999
+
+    }
+
+
+}
