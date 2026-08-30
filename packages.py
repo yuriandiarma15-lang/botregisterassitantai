dc2b4ba14113dff9282752692c1e6da1236df6e9
yuriandiarma15-lang <yuriandiarma15@gmail.com>
2026-08-31 00:39:54 +0700

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,51 +1,23 @@
 # packages.py
-
-
 PACKAGE_MAP = {
-
-
     "1month": {
-
         "label": "1 Bulan",
-
         "price": 299000,
-
         "days": 30
-
     },
-
-
     "6month": {
-
         "label": "6 Bulan",
-
         "price": 500000,
-
         "days": 180
-
     },
-
-
     "12month": {
-
         "label": "12 Bulan",
-
         "price": 850000,
-
         "days": 365
-
     },
-
-
     "permanent": {
-
         "label": "3 tahun",
-
         "price": 1500000,
-
         "days": 1095
-
     }
-
-
 }
